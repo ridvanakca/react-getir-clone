348e0471d295d1c119d50734ca39b41ccb3528a7
Rıdvan Akca <akca.ridvann@gmail.com>
2022-03-28 15:23:35 +0300

    readme is added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,41 @@
-import { BiGlobe } from 'react-icons/bi';
-import { RiUserFill, RiUserAddFill } from 'react-icons/ri';
+import { BiGlobe } from "react-icons/bi";
+import { RiUserFill, RiUserAddFill } from "react-icons/ri";
+import { HiLocationMarker } from "react-icons/hi";
+import { IoIosArrowForward } from "react-icons/io";
 
 function Header() {
   return (
-    <div className='bg-brand-color'>
-      <div className='container mx-auto  h-16 md:h-11 flex items-center justify-center md:justify-between'>
+    <>
+      <div className='bg-brand-color'>
+        <div className='container mx-auto  h-16 md:h-11 flex items-center justify-center md:justify-between'>
+          <a href='#'>
+            <img className='h-10' src={process.env.PUBLIC_URL + "/logo.png"} />
+          </a>
 
-        <a href='#'>
-         <img className='h-10' src={process.env.PUBLIC_URL + '/logo.png'} /> 
-        </a>
-
-        <nav className='hidden md:flex gap-x-8 text-sm font-semibold'>
-          <a href='#' className='flex items-center gap-x-2 text-white transition-all text-opacity-80 hover:text-opacity-100'>
-            <BiGlobe size={20} />
-            Türkçe (TR)
-          </a>
-          <a href='#' className='flex items-center gap-x-2 text-white transition-all text-opacity-80 hover:text-opacity-100'>
-            <RiUserFill size={20}/>
-            Giriş Yap
-          </a>
-          <a href='#' className='flex items-center gap-x-2 text-white transition-all text-opacity-80 hover:text-opacity-100'>
-            <RiUserAddFill size={20}/>
-            Kayıt ol
-          </a>
-        </nav>
+          <nav className='hidden md:flex gap-x-8 text-sm font-semibold'>
+            <a href='#' className='flex items-center gap-x-2 text-white transition-all text-opacity-80 hover:text-opacity-100'>
+              <BiGlobe size={20} />
+              Türkçe (TR)
+            </a>
+            <a href='#' className='flex items-center gap-x-2 text-white transition-all text-opacity-80 hover:text-opacity-100'>
+              <RiUserFill size={20} />
+              Giriş Yap
+            </a>
+            <a href='#' className='flex items-center gap-x-2 text-white transition-all text-opacity-80 hover:text-opacity-100'>
+              <RiUserAddFill size={20} />
+              Kayıt ol
+            </a>
+          </nav>
+        </div>
       </div>
-    </div>
+      <div className='container mx-auto flex md:hidden items-center h-10 px-2 justify-between'>
+        <div className='flex items-center gap-x-2 text-sm font-semibold text-gray-800'>
+          <HiLocationMarker size={18} className='text-primary-brand-color' />
+          Teslimat Adresi Belirle
+        </div>
+        <IoIosArrowForward size={18} className='text-primary-brand-color' />
+      </div>
+    </>
   );
 }
 
